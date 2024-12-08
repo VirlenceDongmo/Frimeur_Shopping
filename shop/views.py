f5d73fbeb808052bb7341e5c7abc2156c7511056
Virlence5 <dongmovirlence@gmail.com>
2024-12-08 14:18:00 +0100

    Envoi d'emails ok

--- shop/views.py
+++ shop/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required
+import json
 
 # Create your views here.
 
@@ -49,35 +50,86 @@ def checkout(request) :
         com = Commande(items = items, total = total, nom = nom, email = email, adresseDeLivraison = adresseDeLivraison, ville = ville, pays = pays, tel = tel, paiement = paiement, user_id = user)
         com.save()
 
+        # Convertir les items du panier (JSON) en dictionnaire
+        panier = json.loads(items)
+
+        # Préparer les détails de chaque article pour l'e-mail
+        article_details = []
+        for item_id, details in panier.items():
+            nom_article = details[1]
+            quantite = details[0]
+            prix_unitaire = details[2]
+            image = details[3]
+            prix_total = quantite * prix_unitaire
+            
+            # Ajoutez les informations à la liste
+            article_details.append(f"""*{nom_article} :
+                            - Photo : {image},
+                            - Quantité: {quantite},
+                            - Prix Total: {prix_total:.2f} FCFA""")
+
+        # Convertir la liste en chaîne
+        articles_str = "\n".join(article_details)
+
         # Envoyer un email
 
-        subject = 'Nouvelle commande'
-        message = f"""
+        subjectForUs = 'Nouvelle commande'
+        messageForUs = f"""
         Une nouvelle commande a été passée par {nom} depuis Frimeur-Shopping.
 
         Détails de la commande :
 
-        - Email du commanditaire : {email}
+        - Email du commanditaire : 
+            {email}
 
-        - Adresse de livraison : {adresseDeLivraison}
+        - Adresse de livraison : 
+            {adresseDeLivraison}
 
-        - Ville de résidence du commanditaire : {ville}
+        - Ville de résidence du commanditaire : 
+            {ville}
 
-        - Pays de résidence du commanditaire : {pays}
+        - Pays de résidence du commanditaire : 
+            {pays}
 
-        - Numéro de téléphone du commanditaire : {tel}
+        - Numéro de téléphone du commanditaire : 
+            {tel}
 
-        - Somme totale des commandes : {total}
+        - Somme totale des commandes : 
+            {total}
 
-        - Mode de paiement : {paiement}
+        - Mode de paiement : 
+            {paiement}
 
-        - Articles : {items}
+        - Articles : 
+        {articles_str}
 
         """
+
         from_email = 'dongmovirlence@gmail.com'
         recipient_list = ['dongmofeudjio5@gmail.com']
 
-        send_mail(subject, message, from_email, recipient_list)
+        send_mail(subjectForUs, messageForUs, from_email, recipient_list)
+
+        subjectForHer = 'Demande de confirmation depuis Frimeur-Shopping'
+
+        messageForHer = f"""
+        Nous vous sommes reconnaissants pour votre confiance Mr/Mme/Mslle {nom}.
+        
+        Si vous rencontrez des problèmes pour vos opérations sur notre plateforme, à défaut de nous contacter par email, vous pouvez le faire via notre numéro whatsapp suivant : 6 54 15 81 75 .
+        """
+
+        if paiement == 'MTN Mobile Money' :
+            messageForHer += f"""
+            Pour confirmer votre commande, veuillez verser la somme de : {total} au numero de compte suivant : 6 54 15 81 75 avec pour nom Dongmo virlence."""
+
+        if paiement == 'Orange Money' :
+            messageForHer += f"""
+            Pour confirmer votre commande, veuillez verser la somme de : {total} au numero de compte suivant : 6 56 93 19 87 avec pour nom Feudjio Divin."""
+
+        from_email = 'dongmovirlence@gmail.com'
+        recipient_list = {email}
+
+        send_mail(subjectForHer,messageForHer,from_email,recipient_list)
 
         return redirect('confirmation')
 
