cc515e4803f557013994370282fcdc68b0e062b2
Virlence5 <dongmovirlence@gmail.com>
2025-01-06 15:49:14 +0100

    Configuration de la base de données avec le SGBD Postgresql.

--- FrimeurShopping/settings.py
+++ FrimeurShopping/settings.py
@@ -80,10 +80,32 @@ WSGI_APPLICATION = 'FrimeurShopping.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'frimeurShoppingDB',  
+#        'USER': 'root',        
+#        'PASSWORD': '',        
+#        'HOST': '127.0.0.1',  
+#        'PORT': '3306',       
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'frimeurshoppingdb',
+        'USER': 'postgres',  
+        'PASSWORD': 'virlence444', 
+        'HOST': 'localhost',  
+        'PORT': '5432',  
     }
 }
 
